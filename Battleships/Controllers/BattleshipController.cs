cb73d2e6c328b97ee7971687da151bc62386b0c3
Maniek13 <mani3k1989@gmail.com>
2021-04-29 21:37:13 +0200

    net core

---
+++ Battleships/Controllers/BattleshipController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Battleship.Core;
+
+namespace Battleships.Controllers
+{
+    [Produces("application/json")]
+    public class BattleshipController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome to the battleship backend";
+        }
+
+        // GET: /Battleship/Ships/ 
+        [HttpGet]
+        public JsonResult Ships()
+        {
+            BattleshipCore core = new BattleshipCore();
+
+            var data = core.UsersShips();
+
+            Request.HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            return Json(data);
+        }
+
+        // GET: /Battleship/moves?gameId=1&player=1
+        [HttpGet]
+        public JsonResult Moves(int gameId, int player, bool next)
+        {
+            BattleshipCore core = new BattleshipCore();
+
+            var move = core.ComputerMove(gameId, player, next);
+
+            Request.HttpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            return Json(move);
+        }
+    }
+}
