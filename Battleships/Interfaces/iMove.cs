6f79c6e93b3f1477b4adc87758fb9a5319dca1c1
Maniek13 <mani3k1989@gmail.com>
2021-05-05 13:03:23 +0200

    remove

--- Battleships/Interfaces/iMove.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Battleships.Interfaces
-{
-    interface IMove
-    {
-        int Message { get; set; }
-        int Hit { get; set; }
-        int Field { get; set; }
-    }
-}
--- README.md
+++
@@ -1,6 +0,0 @@
-# Batleship bacckend ASP.NET MVC
-
-
-1. Create new project: ASP.NET Core (Model-View-Controller)
-2. Copy files from repo
-3. Start server
